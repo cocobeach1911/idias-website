3ef5c555555ae33373bd6ab52f670a2ceb8583a3
wph95 <2732352+wph95@users.noreply.github.com>
2024-12-13 13:52:15 -0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,99 @@
-import Image from "next/image";
+import {NavigationMenuDemo} from "@/components/home/nav";
+import {Button} from "@/components/ui/button";
+
+const data = {
+  main: {
+    title: "Reimagine data lake search and analytics",
+    description: "Index your vector, text, geospatial and JSON data for the most efficient hybrid search and real-time analytics at any scale",
+  },
+}
+
+
+
+const Nav = () => {
+  return (
+    <nav className="z-20 mx-auto w-full px-3 relative max-w-screen-lg lg:px-4 xl:px-0">
+      <div className="flex h-14 items-center justify-between">
+        <div className="font-mono font-bold text-blue-600">
+          Idias
+        </div>
+        <NavigationMenuDemo/>
+        <div>
+          <Button size="sm" className="font-semibold">Join waitlist</Button>
+        </div>
+      </div>
+
+    </nav>
+  )
+}
+
+const MainSection = () => {
+
+  return (
+    <div className="relative mx-auto mt-4 w-full max-w-screen-lg overflow-hidden rounded-2xl bg-neutral-50 p-6  sm:p-12 sm:px-12">
+
+
+      <svg className="pointer-events-none absolute inset-[unset] left-1/2 top-0 w-[1200px] -translate-x-1/2 text-neutral-300 [mask-image:linear-gradient(transparent,black_70%)]" width="100%" height="100%"><defs><pattern id="grid-:R137qnja:" x="0" y="-53.5" width="80" height="80" patternUnits="userSpaceOnUse"><path d="M 80 0 L 0 0 0 80" fill="transparent" stroke="currentColor" stroke-width="1"></path></pattern></defs><rect fill="url(#grid-:R137qnja:)" width="100%" height="100%"></rect></svg>
+      <div className="z-10 relative">
+        <h1 className="text-4xl font-semibold">{data.main.title}</h1>
+        <h4  className="text-2xl pt-3">{data.main.description}</h4>
+        <div className="pt-24 font-semibold flex gap-2">
+          <Button size="sm" className="font-semibold">Join waitlist</Button>
+          <Button variant="outline" className="">Read the whitepaper</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const FeatureSection = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold ">Powerful features for <span className="font-mono font-semibold text-blue-600">Idias</span> Search Lake</h1>
+
+      <div className="grid grid-cols-2 gap-8 pt-4">
+
+        <div className="relative h-full break-inside-avoid rounded-2xl border border-neutral-200/50 bg-neutral-50 [--accent-color:#20808D]">
+          <div className="px-8 py-4">
+              <div>
+                <span className="font-bold underline underline-offset-2 pr-1.5">
+                  100%
+                </span>
+                compatibility with Elasticsearch/OpenSearch
+              </div>
+          </div>
+        </div>
+
+        <div className="relative h-full break-inside-avoid rounded-2xl border border-neutral-200/50 bg-neutral-50 [--accent-color:#20808D]">
+          <div className="px-8 py-4">
+            <div>
+              Serverless Architecture. Scale without limits.
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="w-full">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+      <Nav/>
+
+      <div className=" mx-auto w-full px-3 relative max-w-screen-lg lg:px-4 xl:px-0">
+        <div className="flex gap-16 flex-col">
+          <MainSection />
+
+          <FeatureSection />
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      </div>
     </div>
   );
 }
